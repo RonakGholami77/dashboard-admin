c42c79908ec39ffbfd843ab8516b910b15bd46b3
Rgh <gholamironak88@yahoo.com>
2025-01-18 00:42:58 +0330

    Implement feature cards at home paage

---
+++ src/pages/Home/Features/Feature.jsx
@@ -0,0 +1,26 @@
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+
+function Feature(props) {
+  const { title, money, rate, arrow, detailes } = props;
+
+  return (
+    <div className="layoutCard">
+      <span className="featureTitle">{title}</span>
+      <div className="featureprice">
+        <span className="featureMoney">${money}</span>
+        <span className="featureRate">
+          {rate}{" "}
+          {arrow === "down" ? (
+            <ArrowDownwardIcon className="arrowDown" />
+          ) : (
+            <ArrowUpwardIcon className="arrowUp" />
+          )}
+        </span>
+      </div>
+      <span className="featureDetail">{detailes}</span>
+    </div>
+  );
+}
+
+export default Feature;
---
+++ src/pages/Home/Features/Features.jsx
@@ -0,0 +1,37 @@
+import Feature from "./Feature";
+
+function Features() {
+  const featuresData = [
+    {
+      title: "Revanue",
+      money: "2,415",
+      rate: "-11.04",
+      arrow: "down",
+      detailes: "Compared to last month",
+    },
+    {
+      title: "Sales",
+      money: "4,415",
+      rate: "-1.04",
+      arrow: "down",
+      detailes: "Compared to last month",
+    },
+    {
+      title: "Cost",
+      money: "2,225",
+      rate: "+2.4",
+      arrow: "up",
+      detailes: "Compared to last month",
+    },
+  ];
+
+  return (
+    <div className="layout">
+      {featuresData.map((feature) => {
+        return <Feature {...feature} />;
+      })}
+    </div>
+  );
+}
+
+export default Features;
